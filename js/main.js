2d98d183b18809287ff213b6da6a35b937e5bd45
Kate Hsiao <katehsiao@ischool.berkeley.edu>
2013-12-09 00:47:35 -0800

    Display name outside the circle

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 // Set svg size and margin
 var margin = {top: 20, right: 80, bottom: 80, left: 80},
     width = 960 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+    height = 700 - margin.top - margin.bottom;
 
 // Initial variables for storing categories
-var arrayTime = ["2012-Q1", "2012-Q2"];
+var arrayTime = ["2013-Q3", "2013-Q4"];
 var currentCategory = "relationship-like";
-var currentTime = "2012-Q1";
+var currentTime = "2013-Q3";
 
 // Start drawing
 var svg = d3.select("#viz-container").append("svg")
@@ -17,8 +17,16 @@ var svg = d3.select("#viz-container").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // First time loading
-updateGraph();
+// updateGraph();
+firstTimeGraph();
 
+var feed = [];
+var feedScale;
+var user_feed = [];
+var user_circle;
+var friend_circle_g;
+var friend_circle;
+var friend_name;
 
 // Add event listener
 $(".timerange").on("click", function(){
@@ -44,26 +52,26 @@ function updateCategory(category_num) {
 }
 
 
-function updateGraph() {
+function firstTimeGraph() {
 
   // Normalize feed count to draw radius
   // Use feedScale() to normalize 
-  var feed = [];
+  feed = [];
   for (var i=0; i<arrayTime.length; i++) {
     // push user feedCount
-    feed.push(data[currentCategory][arrayTime[i]]["user"].feedCount);
+    feed.push(all_data[currentCategory][arrayTime[i]]["user"].feedCount);
     // push friend feedCount
-    for (var k=0; k<data[currentCategory][arrayTime[i]]["friend"].length; k++) {
-      feed.push(data[currentCategory][arrayTime[i]]["friend"][k].feedCount);
+    for (var k=0; k<all_data[currentCategory][arrayTime[i]]["friends"].length; k++) {
+      feed.push(all_data[currentCategory][arrayTime[i]]["friends"][k].feedCount);
     }
   }
-  var feedScale = d3.scale.linear()
+  feedScale = d3.scale.linear()
     .domain([d3.min(feed), d3.max(feed)])
     .range([5,20]);
 
   // Create User circle data
-  var user_feed = [];
-  user_feed.push(data[currentCategory][currentTime]["user"].feedCount);
+  user_feed = [];
+  user_feed.push(all_data[currentCategory][currentTime]["user"].feedCount);
 
   // Create Friend circle data
   // Normalize the size and distance of the circles
@@ -90,7 +98,7 @@ function updateGraph() {
 
   // Draw the circles
   // User circle
-  var user_circle = svg.selectAll("user_circle")
+  user_circle = svg.selectAll("user_circle")
     .data(user_feed)
     .enter().append("circle")
     .attr({
@@ -104,24 +112,148 @@ function updateGraph() {
 
   // Friend circles
   // currently use like only
-  var friend_circle = svg.selectAll("friend_circle")
-    .data(data[currentCategory][currentTime]["friend"])
-    .enter().append("circle")
+
+  friend_circle_g = svg.selectAll("friend_circle_g")
+    .data(all_data[currentCategory][currentTime]["friends"])
+    .enter().append("g")
+    // .attr("text-anchor", "middle")
     .attr({
-      "cx": function(d,i) {
-        return (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.cos((Math.PI*2)/data[currentCategory][currentTime]["friend"].length*(i)) + width/2;
-      },
-      "cy": function(d,i) {
-        return (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.sin((Math.PI*2)/data[currentCategory][currentTime]["friend"].length*(i)) + height/2;
-      },
+      "transform": function(d,i) {
+        var x = (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.cos((Math.PI*2)/all_data[currentCategory][currentTime]["friends"].length*(i)) + width/2;
+        var y = (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.sin((Math.PI*2)/all_data[currentCategory][currentTime]["friends"].length*(i)) + height/2;
+        var rotation = 360/all_data[currentCategory][currentTime]["friends"].length*(i);
+        if (rotation>90 && rotation<270) {
+          rotation = rotation+180;
+          d3.select(this).style("text-anchor", "end");
+        }
+        return "translate("+x+","+y+") rotate("+rotation+")";
+      }
+    });
+    // .on("mouseover", function(d,i) {
+    //     d3.select(this).style("opacity",0.8);
+    //     // console.log(d.name);
+    // });
+  friend_circle = friend_circle_g.append("circle")
+    .attr({
+      "cx": 0,
+      "cy": 0,
       "r": function(d,i) {
         return feedScale(d.feedCount);
       },
       "fill": "#000"
     });
 
-  // if 5 data:
-  // 360/5 -> (Math.PI*2)/5
+  friend_name = friend_circle_g.append("text")
+    .attr({
+      "x": function(d,i) {
+        var rotation = 360/all_data[currentCategory][currentTime]["friends"].length*(i);
+        if (rotation>90 && rotation<270) return -25;
+        else return 25;
+      },
+      "y": 5,
+      "fill": "#000",
+    })
+    .text(function(d,i) {
+      return d.name;
+    });
 }
 
+function updateGraph() {
+  feed = [];
+  for (var i=0; i<arrayTime.length; i++) {
+    // push user feedCount
+    feed.push(all_data[currentCategory][arrayTime[i]]["user"].feedCount);
+    // push friend feedCount
+    for (var k=0; k<all_data[currentCategory][arrayTime[i]]["friends"].length; k++) {
+      feed.push(all_data[currentCategory][arrayTime[i]]["friends"][k].feedCount);
+    }
+  }
+  feedScale = d3.scale.linear()
+    .domain([d3.min(feed), d3.max(feed)])
+    .range([5,20]);
 
+  // Create User circle data
+  user_feed = [];
+  user_feed.push(all_data[currentCategory][currentTime]["user"].feedCount);
+
+
+  // Draw the circles
+  // User circle
+  user_circle.data(user_feed)
+    .enter().append("circle")
+    .attr({
+      "cx": width/2,
+      "cy": height/2,
+      "r": function(d,i) {
+        return feedScale(d);
+      },
+      "fill": "#f00"
+    });
+
+  user_circle.transition()
+    .duration(400)
+    .attr({
+      "cx": width/2,
+      "cy": height/2,
+      "r": function(d,i) {
+        return feedScale(d);
+      }
+    });
+
+  // Friend circles
+  // currently use like only
+
+  friend_circle_g.data(all_data[currentCategory][currentTime]["friends"])
+    .enter().append("g")
+    .attr({
+      "transform": function(d,i) {
+        var x = (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.cos((Math.PI*2)/all_data[currentCategory][currentTime]["friends"].length*(i)) + width/2;
+        var y = (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.sin((Math.PI*2)/all_data[currentCategory][currentTime]["friends"].length*(i)) + height/2;
+        var rotation = 360/all_data[currentCategory][currentTime]["friends"].length*(i);
+        if (rotation>90 && rotation<270) {
+          rotation = rotation+180;
+          d3.select(this).style("text-anchor", "end");
+        }
+        return "translate("+x+","+y+") rotate("+rotation+")";
+      }
+    });
+    // .on("mouseover", function(d,i) {
+    //     d3.select(this).style("opacity",0.8);
+    //     // console.log(d.name);
+    // });
+
+  friend_circle = friend_circle_g.selectAll("circle")
+    .data(function(d,i) { return d; });
+  friend_circle.exit().transition(400)
+    .attr("r",0)
+    .remove();
+
+  friend_circle = friend_circle_g.append("circle").transition()
+    .duration(400)
+    .attr({
+      "cx": 0,
+      "cy": 0,
+      "r": function(d,i) {
+        return feedScale(d.feedCount);
+      },
+      "fill": "#000"
+    });
+
+
+  // Transition
+  friend_circle_g.transition()
+    .duration(400)
+    .attr({
+      "transform": function(d,i) {
+        var x = (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.cos((Math.PI*2)/all_data[currentCategory][currentTime]["friends"].length*(i)) + width/2;
+        var y = (200*(1-d.likeCount/user_feed[0])+2*feedScale(user_feed[0]))*Math.sin((Math.PI*2)/all_data[currentCategory][currentTime]["friends"].length*(i)) + height/2;
+        var rotation = 360/all_data[currentCategory][currentTime]["friends"].length*(i);
+        if (rotation>90 && rotation<270) {
+          rotation = rotation+180;
+          d3.select(this).style("text-anchor", "end");
+        }
+        return "translate("+x+","+y+") rotate("+rotation+")";
+      }
+    });
+
+}
